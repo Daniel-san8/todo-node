4fd5c1fa23dae59156f983bcd727771f48dd1dce
Daniel San <danielgdt88@hotmail.com>
2025-05-11 00:48:29 -0300

    feat: create route post

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,4 +1,7 @@
 import { FastifyInstance } from 'fastify';
+import knex from 'knex';
+import { randomUUID } from 'node:crypto';
+import { z } from 'zod';
 
 export default async function tasksRoutes(app: FastifyInstance) {
   app.get('/', async (req, reply) => {
@@ -6,4 +9,41 @@ export default async function tasksRoutes(app: FastifyInstance) {
       message: 'Tudo correto!!',
     });
   });
+
+  app.post(
+    '/',
+    {
+      onError: (_req, reply) => {
+        reply.status(500).send({
+          error: 'Erro ao criar tarefa',
+        });
+      },
+    },
+    async (req, reply) => {
+      const bodyFormat = z.object({
+        name: z.string(),
+        description: z.string(),
+      });
+
+      const { name, description } = bodyFormat.parse(req.body);
+
+      if (!name || !description) {
+        return reply.status(400).send({
+          error: 'Requisição inválida!',
+        });
+      }
+
+      await knex('tasks').insert({
+        id: randomUUID(),
+        author_id: randomUUID(),
+        name,
+        description,
+        created_at: new Date(),
+      });
+
+      reply.status(201).send({
+        message: 'Tarefa criada com sucesso!',
+      });
+    }
+  );
 }
